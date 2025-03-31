7917a883ea37fabadc2d3a71bbb22042c266a852
Caleb Stott <caleb.stott@outlook.com>
2025-03-31 16:42:46 +0100

    Removed some comments and added mongo status check

--- scripts/mongo_db_deploy.sh
+++ scripts/mongo_db_deploy.sh
@@ -28,25 +28,9 @@ sudo apt-get install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org
 
 # change mongoDB config
 sudo sed -i '21c\  bindIp: 0.0.0.0' /etc/mongod.conf
-# manual steps
-# sudo nano /etc/mongod.conf
-# change bindIP line to 0.0.0.0 (line 21)
 
 # start mongoDB
 sudo systemctl start mongod
 
-# All of this inside app script
-# create environment variable for mongoDB - can make it persistent by adding to .bashrc file
-# export DB_HOST=mongodb://<db-ip-address>:27017/posts
-
-# check the env variable has been set
-# printenv DB_HOST
-
-# install dependencies
-# sudo npm install
-
-# seed the database
-# node seeds/seed.js
-
-# run the app
-# pm2 start app.js &
+# check mongoDB status
+sudo systemctl status mongod
